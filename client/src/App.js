ff3a2b482713da60554b94884b8d85107c89b597
mpaccione <mpaccione1991@gmail.com>
2021-05-06 12:54:05 -0700

    Initial UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import theme from "styled-theming";
+import IssueSubmission from "@/components/issue-submission/index.jsx";
+import { Radio } from "semantic-ui-react";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+
+const backgroundColor = theme("mode", {
+  light: "#fefefe",
+  dark: "#282c34",
+});
+
+const Application = styled.div`
+  background-color: ${backgroundColor};
+`;
+
+const initialMode = localStorage.getItem('mode')
+
 
 function App() {
+  const [showBugList, setShowBugList] = useState();
+  const [mode, setMode] = useState(initialMode !== null ? initialMode : "light");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={{ mode: mode }}>
+      <Application className="App">
+        <Radio slider onChange={() => {
+          const newMode = mode === "light" ? "dark" : "light"
+          localStorage.setItem('mode', newMode)
+          setMode(newMode)
+        }} />
+        {showBugList ? <div>Bug List Goes Here</div> : <IssueSubmission />}
+        <Radio
+          slider
+          onChange={(e, { value }) => {
+            setShowBugList(!showBugList);
+          }}
+        />
+      </Application>
+    </ThemeProvider>
   );
 }
 
