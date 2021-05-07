7937b14562863b0010b54e6b0def1ae2105e515a
mpaccione <mpaccione1991@gmail.com>
2021-05-06 22:32:41 -0700

    Helpers and Basic Server

---
+++ server/src/routes/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+// import { submission } from "../controllers/submissionController.js";
+
+const app = express();
+
+// app.use('/auth', authRoutes )
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+// SUBMISSION
+// app.post("/api/submission", submission);
+
+
+export default app;
